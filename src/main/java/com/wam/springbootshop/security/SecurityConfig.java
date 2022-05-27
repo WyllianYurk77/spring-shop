52fc16aa5dea9a35ea040bff38ac6fe957a2bb6b
Wyllian Yurk <wyllian.mendes@dcx.ufpb.br>
2022-05-27 00:39:16 -0300

    Adding JWT authorizaion filter

--- src/main/java/com/wam/springbootshop/security/SecurityConfig.java
+++ src/main/java/com/wam/springbootshop/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.wam.springbootshop.security;
 
+import com.wam.springbootshop.security.jwt.JwtAuthorizationFilter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -44,12 +47,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
             .antMatchers("/api/authentication/**").permitAll()
             .anyRequest().authenticated();
+        http.addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
     @Bean(BeanIds.AUTHENTICATION_MANAGER)
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public JwtAuthorizationFilter jwtAuthorizationFilter() {
+        return new JwtAuthorizationFilter();
     }
 
     @Bean
